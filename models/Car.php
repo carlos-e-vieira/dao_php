a8909987d8ce1f3a0331584f729a4414d3fa2333
Carlos Vieira <carlosportalsocial@gmail.com>
2023-07-18 10:25:52 -0300

    added car model

---
+++ models/Car.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+class Car
+{
+    private $id;
+    private $brand;
+    private $km;
+    private $color;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getBrand(): string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand($brand): void
+    {
+        $this->brand = $brand;
+    }
+
+    public function getKm(): float
+    {
+        return $this->km;
+    }
+
+    public function setKm($km): void
+    {
+        $this->km = $km;
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function setColor($color): void
+    {
+        $this->color = $color;
+    }
+}
