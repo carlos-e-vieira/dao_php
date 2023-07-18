ccbffd65e020019aa36457e15b20f153375ebc74
Carlos Vieira <carlosportalsocial@gmail.com>
2023-07-18 11:31:33 -0300

    added method create

--- dao/CarDAO.php
+++ dao/CarDAO.php
@@ -4,9 +4,28 @@ include_once('models/Car.php');
 
 class CarDAO implements CarDAOInterface
 {
+    private $conn;
+
+    public function __construct(PDO $conn)
+    {
+        $this->conn = $conn;
+    }
+
     public function create(Car $car)
     {
+        $query = 'INSERT INTO cars (brand, km, color) VALUES (:brand, :km, :color)';
 
+        $stmt = $this->conn->prepare($query);
+        
+        $stmt->bindParam(':brand', $car->getBrand());
+        $stmt->bindParam(':km', $car->getKm());
+        $stmt->bindParam(':color', $car->getColor());
+
+        try {
+            $stmt->execute();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
 
     public function findAll()
--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
-    include_once('db.php');
+    //include_once('db.php');
 ?>
+
+<h1>Insira um carro:</h1>
+<form action="process.php" method="POST">
+    <div>
+        <label for="brand">Marca do Carro:</label>
+        <input type="text" name="brand" placeholder="Insira a marca">
+    </div>
+
+    <div>
+        <label for="km">Kilometragem:</label>
+        <input type="text" name="km" placeholder="Insira o km">
+    </div>
+
+    <div>
+        <label for="color">Cor do Carro:</label>
+        <input type="text" name="color" placeholder="Cor do carro">
+    </div>
+    <input type="submit" value="Salvar">
+</form>
